0755bb2da07f385e77a9005ac0599ca895647587
Yousef-Elbeyaly <yousef.amir080@gmail.com>
2026-08-01 18:00:30 +0300

    update validation and architecture

--- src/routes/data.py
+++ src/routes/data.py
@@ -29,7 +29,7 @@ async def upload_data(project_id: str, file: UploadFile,
         )
 
     project_dir_path = ProjectController().get_project_path(project_id=project_id)
-    file_path = data_controller.generate_unique_filename(orig_file_name=file.filename, project_id = project_id)
+    file_path, file_id = data_controller.generate_unique_filepath(orig_file_name=file.filename, project_id = project_id)
 
     try:      
         async with aiofiles.open(file_path, "wb") as f:
@@ -46,6 +46,7 @@ async def upload_data(project_id: str, file: UploadFile,
     
     return JSONResponse(
         content={
-            "signal": ResponseSignal.FILE_UPLOAD_SUCCESS.value
+            "signal": ResponseSignal.FILE_UPLOAD_SUCCESS.value,
+            "file_id": file_id
                  })
     
