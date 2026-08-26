08be48206e19c66bb8988b7cbbf6fe0a26283d76
Yousef-Elbeyaly <yousef.amir080@gmail.com>
2026-08-26 17:32:58 +0300

    Creating rabbitmq nad redis

--- src/helpers/config.py
+++ src/helpers/config.py
@@ -1,4 +1,4 @@
-from pydantic_settings import BaseSettings, SettingsConfigDict #type: ignore
+from pydantic_settings import BaseSettings, SettingsConfigDict
 from typing import List
 
 class Settings(BaseSettings):
@@ -41,7 +41,14 @@ class Settings(BaseSettings):
 
     DEFAULT_LANG: str = "en"
     PRIMARY_LANG: str = "ar"
-    
+
+    CELERY_BROKER_URL: str = None
+    CELERY_RESULT_BACKEND: str = None
+    CELERY_TASK_SERIALIZER: str= "json"
+    CELERY_TASK_TIME_LIMIT: int = 600
+    CELERY_TASK_ACKS_LATE: bool = False
+    CELERY_WORKER_CONCURRENCY: int = 2
+        
     class Config:
         env_file = ".env"
 
--- src/routes/base.py
+++ src/routes/base.py
@@ -1,6 +1,11 @@
 from fastapi import FastAPI, APIRouter, Depends # type: ignore
 import os
 from helpers.config import get_settings, Settings
+from time import sleep
+import logging
+from tasks.mail_service import send_email_reports
+
+logger = logging.getLogger('uvicorn.error')
 
 base_router = APIRouter(
     prefix = "/api/v1",
@@ -17,4 +22,14 @@ async def welcome(app_settings: Settings = Depends(get_settings)):
     return{
         "APP_NAME": app_name,
         "APP_VERSION": app_version
+    }
+
+@base_router.get("/send_reports")
+async def send_reports(app_settings: Settings = Depends(get_settings)):
+
+    task = send_email_reports.delay(mail_wait_seconds=3)
+
+    return {
+        "success": True,
+        "task_id": task.id
     }
